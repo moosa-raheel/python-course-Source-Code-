11035c0e5823fb0e02f5d99165b65cb6e64ecc8c
moosa-raheel <moosarahil13@gmail.com>
2023-11-04 20:53:12 +0500

    video10 complete

---
+++ chapter 2/escape_sequences.py
@@ -0,0 +1,25 @@
+#Escape Sequences
+
+#\' => '
+print('Hy I\'m Programmer')
+
+#\" => "
+print(" i ma \"programmer\"")
+print("I\'m Coder")
+
+#\n => new Line
+print("Twinkle, twinkle, little star,\nHow I wonder what you are!")
+print("Up above the world so high,")
+print("Like a diamond in the sky.")
+
+#\t => tab        
+print("\n\nDear [Supervisor\'s Name],")
+print("\tI am writing to request a leave of absence from [start date] to [end date] due to [briefly state the reason]. Please consider my request, and I will ensure a smooth handover of my responsibilities.")
+print("Sincerely,")
+print("[Your Name]")
+
+#\\ => \
+print("\n\nThis is double backslash\\\\")
+
+#\b => backspace
+print("\n\nHelll\boo\b")
