0b23ae59e391bab029611704bc1b235582913268
moosa-raheel <moosarahil13@gmail.com>
2023-11-05 16:22:06 +0500

    video11 completed

---
+++ chapter 2/printfunc_keyword_args.py
@@ -0,0 +1,16 @@
+#Print Function Keyword Arguments
+
+# print("I am coder","i am programmer")
+
+# print("Twinkle, twinkle, little star,","How I wonder what you are!","Up above the world so high,","Like a diamond in the sky.",sep="\n")
+
+# By default it seperated from " "
+# But you can change it like sep="\n"
+
+# By defaund end is end="\n"
+# but you can change it like end=" "
+
+print("Twinkle, twinkle, little star,", end=" ")
+print("How I wonder what you are!")
+print("Up above the world so high,")
+print("Like a diamond in the sky.")
