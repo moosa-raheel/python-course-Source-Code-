e741d22e5641682330e96e4bdf5bd9c61c28692e
moosa-raheel <moosarahil13@gmail.com>
2023-11-05 16:26:38 +0500

    video12 completed

---
+++ chapter 2/escape_sequence_normal_text.py
@@ -0,0 +1,18 @@
+# Escape sequence as normal text
+
+# output: \n is a escape sequence
+
+# \\=>\
+print("\\n is a escape sequence")
+print("\\t is a escape sequence")
+
+#output : \" \'
+
+# \" => "
+# \' => '
+print("\\\" \\\'")
+
+# \\=> \
+# \" => "
+# \' => '
+print("\\\" \\\'")
