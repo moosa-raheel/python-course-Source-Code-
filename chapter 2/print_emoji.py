4590dca5a6b66910b3d0ae804c4aceb43219ae2c
moosa-raheel <moosarahil13@gmail.com>
2023-11-06 11:58:17 +0500

    video16 completed

---
+++ chapter 2/print_emoji.py
@@ -0,0 +1,8 @@
+# print emoji 
+
+# Website link : https://unicode.org/emoji/charts/full-emoji-list.html
+
+# Replace the + with 000 of unicode and add \ in the beginning of unicode
+
+print("\U0001F923")
+print("\U0001F607")
