9d19bdbf55786dd29de4c76af806715d7e556f33
moosa-raheel <moosarahil13@gmail.com>
2023-11-08 12:08:31 +0500

    video17 completed

---
+++ chapter 2/calculator.py
@@ -0,0 +1,57 @@
+# Python As a Calculator
+
+
+                    # Operators 
+
+# Operator        Description             Example
+
+#    +              Addition              print(2+3)
+#                                         5
+
+#    -             Subtraction            print(2-3)
+#                                         -1
+
+#     *             Multiplication         print(2*3)
+#                                             6
+    
+#     /             Float Division         print(4/2)
+#                                             2.0
+    
+#     //            Integer Division       print(4//2)
+#                                             2
+    
+#     %             Modulo                  print(6%2)
+#                                             0
+    
+#     **            Exponent                print(2**3)
+#                                             8
+                                        
+# print(4+6)
+# print(4-2)
+# print(4*2)
+# print(5/2)
+# print(5//2)
+# print(5%2)
+# print(2**3)
+# print(2**0.5)
+# print(round(2**0.5,2))
+
+
+                        # Precedence Rule Or Associativity Rule
+                    
+# Operators                                           Precedence
+
+# Parenthese ()                                         Highest
+
+# Exponent                                              Right to Left
+
+# *,/,//,%                                              Left to Right
+
+# +,-                                                   Left to Right
+
+# print(2**3+6)
+# 8+6
+
+# print(2+3**4)
+# 81+2
+# print((2+3)**2)
