d8a0e9d6a7d3eac663753ff31121e56482449190
moosa-raheel <moosarahil13@gmail.com>
2023-11-11 11:16:41 +0500

    video18 completed

---
+++ chapter 2/variables.py
@@ -0,0 +1,41 @@
+# Variables 
+
+# Variable are like container or box where you can hold values 
+
+# name = "Moosa"
+# print(name)
+
+# number = "6"
+# print(type(number))
+# a = 12 #12
+# b = 6 #6
+# c = a+b #12+6
+# print(c)
+
+# Why Python known as dynamic programming language
+
+# name = "Moosa"
+# print(type(name))
+# name = 12
+# print(type(name))
+
+# name = "Moosa Raheel"
+# age = 6
+
+# my name is myname my age is myage
+# print("my name is ",name,"my age is",age)
+
+        # variable nameing rules 
+
+
+# 1) can't start with number but you can use number in the middle or the end
+# 2) Can't contain spaces
+# 3) can't start with special charector or can't use special charector in the middle or the end except underscore(_)
+
+# age1 = 18
+# myage = 65
+# name = 'Moosa'
+
+# Convention 
+# 1) Python recommend us snake case convention
+my_age =55
