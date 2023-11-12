a3974479e7ae80b53612778114baf6dcf628110f
moosa-raheel <moosarahil13@gmail.com>
2023-11-12 11:49:00 +0500

    video19 completed

---
+++ chapter 2/type_casting.py
@@ -0,0 +1,13 @@
+# Type Casting or Type Conversion 
+
+# Type conversion, also known as type casting, refers to the process of changing the data type of a variable or value from one type to another. 
+
+# a = "8"
+# b= "3"
+# c = int(a)+int(b)
+# print(c)
+
+a = 8
+b = 3
+c = str(a)+str(b)
+print(c)
