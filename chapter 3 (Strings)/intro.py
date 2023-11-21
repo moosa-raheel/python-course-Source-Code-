3680d0af4ae1d22499ed1e888afa0cdd209cf9a5
moosa-raheel <moosarahil13@gmail.com>
2023-11-21 15:15:12 +0500

    video23 completed

---
+++ chapter 3 (Strings)/intro.py
@@ -0,0 +1,22 @@
+# Introduction to Strings and multiline comments
+
+'''
+my name is Moosa
+I am coder
+
+
+yugu
+iyy
+78y8
+
+'''
+
+# Strings
+
+# Strings is a Collection of charectors
+
+my_self = '''My name is Moosa
+I am programmer
+My salary is 500crores
+'''
+print(my_self)
